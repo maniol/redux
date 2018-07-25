8c56dd6bf909cdffcc0457f93d3504ec21765308
magda <magdalena.aniol@gmail.com>
2018-07-25 18:45:25 +0200

    add actions.js

---
+++ actions.js
@@ -0,0 +1,54 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+	return {
+		type: ADD_COMMENT,
+		text,
+		id: uuid.v4()
+	}
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+
+function removeComment(id) {
+	return {
+		type: REMOVE_COMMENT,
+		id
+	}
+}
+
+const boundRemoveComment = text => dispatch(removeComment(text));
+
+function editComment(text, id) {
+	return {
+		type: EDIT_COMMENT,
+		text,
+		id
+	}
+}
+
+const boundEditComment = (text, id) => dispatch(editComment(text, id));
+
+function thumbUpComment(id) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id
+	}
+}
+
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+
+function thumbDownComment(id) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id
+	}
+}
+
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
