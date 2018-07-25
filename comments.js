702022074d6c3ea44efc3d5f69b9d1244e42d9c9
magda <magdalena.aniol@gmail.com>
2018-07-25 20:33:22 +0200

    fix switch statement

--- comments.js
+++ comments.js
@@ -1,5 +1,6 @@
 import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions';
 
+
 function comments(state = [], action) {
 	switch(action.type) {
 		case ADD_COMMENT:
@@ -9,34 +10,42 @@ function comments(state = [], action) {
 					votes: 0
 				}
 				, ...state];
-		default:
-			return state;
 		case REMOVE_COMMENT:
 			return [state.filter(comment => comment.id !== action.id)];
-		default:
-			return state;
 		case EDIT_COMMENT:
-			return [state.map(comment => {
+			return state.map(comment => {
 					if (comment.id == action.id)
-						return comment.text = action.text})
-						];
-		default:
-			return state;
+						return {
+							id: comment.id,
+							text: action.text,
+							votes: comment.votes
+						}
+					return comment
+				})
 		case THUMB_UP_COMMENT:
-			return [state.map(comment => {
+			return state.map(comment => {
 					if (comment.id == action.id)
-						return comment.votes += 1})
-						];
-		default:
-			return state;
+						return {
+							id: comment.id,
+							text: comment.text,
+							votes: comment.votes += 1
+							}
+					return comment
+				})
 		case THUMB_DOWN_COMMENT:
-			return [state.map(comment => {
+			return state.map(comment => {
 					if (comment.id == action.id)
-						return comment.votes -= 1 })
-						];
+						return {
+							id: comment.id,
+							text: comment.text,
+							votes: comment.votes -= 1
+							}
+						return comment
+				})
 		default:
 			return state;
 	}
 }
+
 
 export default comments;
